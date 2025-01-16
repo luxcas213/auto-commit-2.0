4a5b64e09d59888af0d1f6b75d26db055e64cc1c
lucas <lgarbate@gmail.com>
2025-01-15 23:16:22 -0300

    lajkldjslkdj

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 import subprocess
 import datetime
-
+import os
 
 def commit():
+    repo_url = "https://github.com/luxcas213/auto-commit-2.0.git"
+    repo_dir = "C:\Users\Lgarb\Downloads\Repos\auto-commit-2.0"
+    
+    if not os.path.isdir(repo_dir):
+        subprocess.run(["git", "clone", repo_url, repo_dir])
 
-    today = datetime.datetime.now().strftime("%m/%d/%Y")
+    os.chdir(repo_dir)
+
+    subprocess.run(["git", "remote", "set-url", "origin", repo_url])
+
+    subprocess.run(["git", "config", "--global", "user.name", "Tu Nombre"])
+    subprocess.run(["git", "config", "--global", "user.email", "tuemail@example.com"])
+
+    today = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     subprocess.run(["git", "commit", "--allow-empty", "-m", "commit :)", "--date", today])
-    subprocess.run(["git", "push"])
 
-
+    subprocess.run(["git", "push", "origin", "master"])
 
 commit()
-
-
