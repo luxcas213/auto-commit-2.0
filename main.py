e25ef4d09ee8570ec53f44977f0897050d05b124
Tu Nombre <tuemail@example.com>
2025-01-15 23:18:16 -0300

    ladksldkjalkjd

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os
 
 def commit():
     repo_url = "https://github.com/luxcas213/auto-commit-2.0.git"
-    repo_dir = "C:\Users\Lgarb\Downloads\Repos\auto-commit-2.0"
+    repo_dir = "C:\\Users\\Lgarb\\Downloads\\Repos\\auto-commit-2.0"
     
     if not os.path.isdir(repo_dir):
         subprocess.run(["git", "clone", repo_url, repo_dir])
@@ -19,6 +19,6 @@ def commit():
     today = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     subprocess.run(["git", "commit", "--allow-empty", "-m", "commit :)", "--date", today])
 
-    subprocess.run(["git", "push", "origin", "master"])
+    subprocess.run(["git", "push"])
 
 commit()
